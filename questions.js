e3f1f0014721be51cc76015d9121b5373b324844
Jlakshan <52228808+Jlakshan@users.noreply.github.com>
2021-04-04 07:48:01 +1200

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,6 +1,6 @@
 questions = [
     {
-        "question": "trollface",
+        "question": "Progressive taxes should be instated to provide for the poor.",
         "effect": {
             "bpowe": 0,
             "bauth": 0,
@@ -13,13 +13,50 @@ questions = [
             "bvalu": 0,
             "bcult": 0,
             "breli": 0,
+            "bowne": 10,
+            "bregu": 5,
+            "bprod": 0,
+        }
+    },
+
+    {
+        "question": "Inheritance is a legitimate form of wealth.",
+        "effect": {
+            "bpowe": 0,
+            "bauth": 0,
+            "bgovt": 0,
+            "borga": 0,
+            "bsize": 0,
+            "bopen": 0,
+            "bnati": 0,
+            "bconf": 0,
+            "bvalu": 0,
+            "bcult": 0,
+            "breli": 0,
+            "bowne": 10,
+            "bregu": 0,
+            "bprod": 0,
+        }
+    },
+    {
+        "question": "A united world government would be beneficial to mankind.",
+        "effect": {
+            "bpowe": 0,
+            "bauth": 0,
+            "bgovt": 0,
+            "borga": -10,
+            "bsize": 0,
+            "bopen": -10,
+            "bnati": -5,
+            "bconf": 0,
+            "bvalu": 0,
+            "bcult": 0,
+            "breli": 0,
             "bowne": 0,
             "bregu": 0,
             "bprod": 0,
         }
     },
-
-
 
 
 
