35e2502357d7a24b8378809064a6bd62b943975b
Jlakshan <52228808+Jlakshan@users.noreply.github.com>
2021-04-04 12:21:18 +1200

    Update questions.js

--- questions.js
+++ questions.js
@@ -57,8 +57,101 @@ questions = [
             "bprod": 0,
         }
     },
-
-
+    {
+        "question": "A good society is a mix of cultures.",
+        "effect": {
+            "bpowe": 0,
+            "bauth": 0,
+            "bgovt": 0,
+            "borga": 0,
+            "bsize": 0,
+            "bopen": 0,
+            "bnati": 0,
+            "bconf": 0,
+            "bvalu": 0,
+            "bcult": -10,
+            "breli": 0,
+            "bowne": 0,
+            "bregu": 0,
+            "bprod": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "bpowe": 0,
+            "bauth": 0,
+            "bgovt": 0,
+            "borga": 0,
+            "bsize": 0,
+            "bopen": 0,
+            "bnati": 0,
+            "bconf": 0,
+            "bvalu": 0,
+            "bcult": 0,
+            "breli": 0,
+            "bowne": 0,
+            "bregu": 0,
+            "bprod": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "bpowe": 0,
+            "bauth": 0,
+            "bgovt": 0,
+            "borga": 0,
+            "bsize": 0,
+            "bopen": 0,
+            "bnati": 0,
+            "bconf": 0,
+            "bvalu": 0,
+            "bcult": 0,
+            "breli": 0,
+            "bowne": 0,
+            "bregu": 0,
+            "bprod": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "bpowe": 0,
+            "bauth": 0,
+            "bgovt": 0,
+            "borga": 0,
+            "bsize": 0,
+            "bopen": 0,
+            "bnati": 0,
+            "bconf": 0,
+            "bvalu": 0,
+            "bcult": 0,
+            "breli": 0,
+            "bowne": 0,
+            "bregu": 0,
+            "bprod": 0,
+        }
+    },
+    {
+        "question": "",
+        "effect": {
+            "bpowe": 0,
+            "bauth": 0,
+            "bgovt": 0,
+            "borga": 0,
+            "bsize": 0,
+            "bopen": 0,
+            "bnati": 0,
+            "bconf": 0,
+            "bvalu": 0,
+            "bcult": 0,
+            "breli": 0,
+            "bowne": 0,
+            "bregu": 0,
+            "bprod": 0,
+        }
+    },
 
 
 
